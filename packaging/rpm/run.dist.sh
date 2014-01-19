9b05a4b0355edba41d15ecefad5ad86f2e05230c
Chris Wright <chrisw@sous-sol.org>
2014-01-18 23:28:19 -0800

    rpm/run.dist.sh: add usage statement
    when executed with invalid cmdline parameter or the new 'help' cmdline
    parameter, print out a usage statement.
    
    Change-Id: I854fecfaa897e9b6d0f85adb84707357d449a20b
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>

--- packaging/rpm/run.dist.sh
+++ packaging/rpm/run.dist.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+usage() {
+    echo -e "Usage:\t$0 base|virt-ovsdb|virt-vtn|virt-opendove|sp|-stop|-status|help\n"
+
+    echo -e "\tbase: base controller edition, good for simple testing"
+    echo -e "\tvirt-ovsdb: virtualization controller edition based on ovsdb"
+    echo -e "\tvirt-vtn: virtualization controller edition based on vtn
+    (not supported yet)"
+    echo -e "\tvirt-opendove: virtualization controller edition based on opendove (not supported yet)"
+    echo -e "\tsp: service provider controller edition (not supported yet)"
+    echo -e "\t-stop: stop the controller"
+    echo -e "\t-status: check if controller is currently running"
+    echo -e "\thelp: generate this help text"
+}
+
 option=$1;
 shift
 
@@ -39,8 +53,13 @@ sp)
     exec /usr/share/opendaylight-controller/run.internal.sh -status
     ;;
 
+help)
+    usage
+    exit 0
+    ;;
 *)
     echo "Invalid option: $option"
+    usage
     ;;
 esac
 
