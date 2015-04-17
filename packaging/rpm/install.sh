a552d9563a21fff1a60493012b454381d61b6bb5
Daniel Farrell <dfarrell@redhat.com>
2015-04-16 20:18:20 -0400

    Force RPM to use Java 1.7, not newer versions
    * CentOS now installs 1.8 by default, which breaks ODL
    * Bumps RPM version to 0.2.3-2
    
    Change-Id: If9bce5f5eb4a4d5cbfdf7e981a379ffdcb3acc89
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- packaging/rpm/install.sh
+++ packaging/rpm/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Update version/path if needed
-rpm_path="./opendaylight-0.2.3-1.noarch.rpm"
+rpm_path="./opendaylight-0.2.3-2.noarch.rpm"
 
 # Install ODL
 echo "Installing ODL from $rpm_path"
