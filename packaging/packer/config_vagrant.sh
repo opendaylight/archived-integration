ff07e47f1f7a1e5d4ba78cc900d492f2ded88f95
Daniel Farrell <dfarrell@redhat.com>
2015-07-02 14:38:25 -0400

    Add Li Docker image, update Vagrant base box to Li
    Builds/configures ODL Docker images via the same pipeline used by
    Packer when creating Vagrant base boxes.
    
    Upgrades the Vagrant base box to Lithium, mostly for free via updates
    to the Ansible role.
    
    Starts with a recent, fresh CentOS 7 image, does low-level provisioning
    via Kickstart, then Packer scripts, then ODL's Ansible role, which uses
    ODL's CBS RPM. The result is post-processed into a Vagrant box and a
    Docker image.
    
    I did a bit of a docs update, but I need to revisit Packer's docs in
    general and do more.
    
    Change-Id: I123e416573ab079c9350998c4f4deed8d02f7568
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>
    (cherry picked from commit 72621754eca503ff5fd76be1ed550335ed6ab94d)

--- packaging/packer/config_vagrant.sh
+++ packaging/packer/config_vagrant.sh
@@ -9,7 +9,3 @@ install -v -o vagrant -g vagrant -m 0700 -d /home/vagrant/.ssh
 curl -o /home/vagrant/.ssh/authorized_keys -kL "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
-
-# Vagrant requires the ability to run sudo without a tty
-# Else, `vagrant up` will fail with: "sorry, you must have a tty to run sudo"
-sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
